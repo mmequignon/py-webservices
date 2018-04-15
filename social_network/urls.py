9447d920448773b18d84c9d300fbc2ff5bebc2ae
Matthieu MÉQUIGNON <matthieu@fwzte.xyz>
2018-04-15 17:48:26 +0200

    [ADD] social_network - Implements token generation at Buddy creation. Implementation of a route that returns token when authentication is successful.

--- social_network/urls.py
+++ social_network/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework.authtoken import views as rest_framework_views
 
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(
+        r'^get_auth_token/$',
+        rest_framework_views.obtain_auth_token, name='get_auth_token'),
     url(r'^$', views.index, name='index'),
 ]
